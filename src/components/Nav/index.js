5632ee5a971b42c0b342d20f3d6c59b1fa927bb5
lrodenyoder <rodenlau000@gmail.com>
2022-05-12 14:08:58 -0500

    20.2.3

---
+++ src/components/Nav/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+function Nav() {
+    const categories = [
+        {
+            name: "Commercial",
+            description: "Photos of grocery stores, food truck, and other commercial projects",
+        },
+        {
+            name: "Portraits",
+            description: "Portraits of people in my life"
+        },
+        {
+            name: "Food",
+            description: "Delicious delicacies"
+        },
+        {
+            name: "Landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+        }
+    ];
+
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+    }
+
+    return (
+        <header>
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="camera">📸</span> Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        //when mapping in JSX, outermost element must have key attribute set to something unique. Helps keep track of items in virtual DOM. Return only one JSX element with each callback
+                        <li className="mx-1"
+                            key={category.name}>
+                            {/* if written categorySelected(category.name), the function would be called on render as well as click */}
+                            <span onClick={() => categorySelected(category.name)}>
+                                {category.name}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
